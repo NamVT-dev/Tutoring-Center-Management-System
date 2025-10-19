34f1ade02c52d60858fae11b17f3d54457f94f7d
NamVT <namvtfs1134@gmail.com>
2025-10-19 14:33:46 +0700

    Remove categories from test routes

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const authController = require("../controllers/authController");
 const testController = require("../controllers/testController");
-const { getCourseCategories } = require("../controllers/courseController");
 const route = express.Router();
-
-route.get("/course-categories", getCourseCategories);
 
 route.use(authController.protect);
 route.use(authController.restrictTo("member"));
