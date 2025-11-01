c56d5f50c9ce26c041f1c9d6a330817518fa034f
NamVT <namvtfs1134@gmail.com>
2025-11-01 21:04:50 +0700

    Embedding Course

--- models/courseModel.js
+++ models/courseModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { embedText } = require("../utils/openAi");
 
 const courseSchema = new mongoose.Schema({
   name: {
@@ -18,8 +19,27 @@ const courseSchema = new mongoose.Schema({
   session: { type: Number, required: true },
   durationInMinutes: { type: Number, required: true },
   imageCover: String,
+  embedding: {
+    type: [Number],
+    select: false,
+  },
 });
 courseSchema.index({ level: 1, category: 1 });
+
+courseSchema.pre("save", async function (next) {
+  if (
+    !this.isModified("name") &&
+    !this.isModified("category") &&
+    !this.isModified("level") &&
+    !this.isModified("price") &&
+    !this.isModified("durationInMinutes")
+  )
+    return next();
+
+  const textToEmbed = `Course: ${this.name}. Category: ${(await this.populate("category", "name")).category?.name}. Level: ${this.level}. Price: ${this.price}. Duration In Minutes: ${this.durationInMinutes}`;
+  this.embedding = await embedText(textToEmbed);
+  next();
+});
 
 const Course = mongoose.model("Course", courseSchema, "courses");
 
