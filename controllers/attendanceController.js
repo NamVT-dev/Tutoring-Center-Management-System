21a545b9dfa05e67c79f16c7f13a8fd7a565ec58
NamVT <namvtfs1134@gmail.com>
2025-11-08 19:37:09 +0700

    make session

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -1,3 +1,27 @@
+const Session = require("../models/sessionModel");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
-exports.createSession = catchAsync(async (req, res, next) => {});
+const getTodayString = () => {
+  return new Date().toISOString().slice(0, 10);
+};
+
+exports.createSession = catchAsync(async (req, res, next) => {
+  const { teacher } = req.user;
+  const { classId } = req.params;
+  const session = await Session.find({
+    teacher: teacher.id,
+    class: classId,
+    startAt: getTodayString(),
+  });
+
+  if (!session)
+    return next(
+      new AppError("Không tìm thấy session của class tương ứng", 404)
+    );
+
+  res.status(201).json({
+    status: "success",
+    data: session,
+  });
+});
--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const attendanceController = require("../controllers/attendanceController");
 const route = express.Router();
 
+route.post("/start-session/:classId", attendanceController.createSession);
 module.exports = route;
