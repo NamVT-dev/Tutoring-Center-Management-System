cdcd36b4e3a9708a64f34a6456910d15886716d7
NamVT <namvtfs1134@gmail.com>
2025-11-26 21:52:06 +0700

    delete log

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -32,7 +32,6 @@ const sendError = (err, req, res) =>
   });
 
 module.exports = (err, req, res, next) => {
-  console.log(err);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
